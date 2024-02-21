d9314a94711d90950baa46d782808cc5cc862ed2
Dao Pham <daopm.dean@gmail.com>
2024-02-21 11:14:28 +0700

    search insert position o log n complexity

--- interview_150/search-insert-position.go
+++ interview_150/search-insert-position.go
@@ -1,13 +1,32 @@
 package interview_150
 
 func searchInsert(nums []int, target int) int {
-	for i, num := range nums {
-		if num < target {
-			continue
+	if len(nums) == 1 {
+		if nums[0] >= target {
+			return 0
 		}
-
-		return i
+		return 1
 	}
 
-	return len(nums)
+	start, end := 0, len(nums)-1
+	for {
+		if start+1 == end {
+			if nums[start] >= target {
+				return start
+			}
+			if nums[end] >= target {
+				return end
+			}
+			return end + 1
+		}
+
+		i := (start + end) / 2
+		if nums[i] < target {
+			start = i
+		} else {
+			end = i
+		}
+	}
+
+	return 0
 }
