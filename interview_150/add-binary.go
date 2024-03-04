e84b8eff4ae7eec979e45e0007153a408cdee459
Dao Pham <daopm.dean@gmail.com>
2024-03-04 16:18:26 +0700

    add binary

--- interview_150/add-binary.go
+++ interview_150/add-binary.go
@@ -1,10 +1,73 @@
 package interview_150
 
-import (
-	"math"
-	"strconv"
-)
+func addBinary(a string, b string) string {
+	additionResult := ""
+	if len(a) < len(b) {
+		a, b = reverseStr(b), reverseStr(a)
+	} else {
+		a, b = reverseStr(a), reverseStr(b)
+	}
 
+	hold := 0
+	for i := range b {
+		if a[i] == '1' && b[i] == '1' {
+			if hold == 1 {
+				additionResult += "1"
+			} else {
+				additionResult += "0"
+				hold = 1
+			}
+		} else if a[i] == '0' && b[i] == '0' {
+			if hold == 1 {
+				additionResult += "1"
+			} else {
+				additionResult += "0"
+			}
+			hold = 0
+		} else {
+			if hold == 1 {
+				additionResult += "0"
+			} else {
+				additionResult += "1"
+			}
+		}
+	}
+
+	for i := len(b); i < len(a); i++ {
+		if a[i] == '0' {
+			if hold == 1 {
+				additionResult += "1"
+				hold = 0
+			} else {
+				additionResult += "0"
+			}
+		} else {
+			if hold == 1 {
+				additionResult += "0"
+			} else {
+				additionResult += "1"
+			}
+		}
+	}
+
+	if hold == 1 {
+		additionResult += "1"
+	}
+
+	return reverseStr(additionResult)
+}
+
+func reverseStr(str string) string {
+	result := ""
+
+	for i := len(str) - 1; i >= 0; i-- {
+		result += string(str[i])
+	}
+
+	return result
+}
+
+/* brute force solution
 func addBinary(a string, b string) string {
 	x, y := stringBinaryToInt(a), stringBinaryToInt(b)
 	return intToStringBinary(x + y)
@@ -27,3 +90,4 @@ func stringBinaryToInt(str string) int {
 func intToStringBinary(i int) string {
 	return strconv.FormatInt(int64(i), 2)
 }
+*/
